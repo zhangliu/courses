a3ba15a3c66b1360aaf2dc2a192d83677191eb55
yunnian.zhang <yunnian.zhang@yintech.cn>
2018-08-04 14:25:51 +0800

    modify

--- python/file/遍历目录/index.py
+++ python/file/遍历目录/index.py
@@ -14,6 +14,6 @@ def readFile(filePath):
   return result
 
 files = readFile('/Users/ytx/Projects/courses')
-for f in files: print f
+for f in files: print(f)
 # 列表生成式：https://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/001431779637539089fd627094a43a8a7c77e6102e3a811000
 # files = [f for f in files]
