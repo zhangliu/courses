4890b4ff3aac51fdc26467ee4d3e22ff4526486c
zhangliu <zhangliuge@yeah.net>
2019-06-30 09:48:13 +0800

    modify

---
+++ ajax/data/responseType.js
@@ -0,0 +1,1 @@
+module.exports = { helloWrold: 'hello world'}
--- ajax/data/ymockcfg.js
+++ ajax/data/ymockcfg.js
@@ -1,14 +1,18 @@
 const fs = require('fs')
 
-module.exports = [{
+module.exports = [
+	{
+		pattern: /.*\/onload$/,
+		respondWith: './test.mockjson'
+	}, {
 	pattern: /(.*?)$/,
-	// respondWith: function(postData, qs, req, res){
-	// 	const filename = qs.url.replace(/.*\/(.*?)$/, '$1')
-	// 	const path = `${process.cwd()}/${filename}.js`
-	// 	if (!fs.existsSync(path)) return `【错误】没有找到文件${path}!`
-	// 	delete require.cache[path]
-	// 	const data = require(path)
-	// 	return data
-	// }
-	respondWith: './test.mockjson'
+	respondWith: function(postData, qs){
+		const filename = qs.url.replace(/.*\/(.*?)$/, '$1')
+		const path = `${process.cwd()}/${filename}.js`
+		if (!fs.existsSync(path)) return `【错误】没有找到文件${path}!`
+		delete require.cache[path]
+		const data = require(path)
+		return data
+	}
+	// respondWith: './test.mockjson'
 }]
